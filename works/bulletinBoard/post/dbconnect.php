98542e0148d13e5abcccf147dde358c6e2b64470
okuda1506 <okuda1506@gmail.com>
2023-06-14 18:16:53 +0900

    一旦全削除

--- works/bulletinBoard/post/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once(__DIR__ . '/db.php');
-try {
-  $db = new PDO($dsn, $user, $password);
-} catch (PDOException $e) {
-  echo 'DB接続エラー：' . $e->getMessage();
-}
--- works/registerapp/database/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../../config/config.php');
-
-$dbName = DB_NAME_REGISTER_APP;
-$user = DB_USER;
-$password = DB_PASSWORD;
-$host = DB_HOST;
-$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
